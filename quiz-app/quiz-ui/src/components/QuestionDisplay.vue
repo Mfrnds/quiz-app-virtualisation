4ce7ba5bb69f0d3e35456afb943e8609250f6ada
Maryline Fernandes <maryline.fernandes@edu.esiee.fr>
2023-05-09 23:34:51 +0200

    cursor + logo

--- quiz-app/quiz-ui/src/components/QuestionDisplay.vue
+++ quiz-app/quiz-ui/src/components/QuestionDisplay.vue
@@ -2,16 +2,14 @@
     <div class="row text-center">
       <h1 class="main-title">{{ question.questionTitle}}</h1>
     </div>
-    <div class="row text-center">
-      <img width="500px" height="250px" v-if="question.questionImage" :src="question.questionImage" />
+    <div class="row text-center my-2">
+      <img width="500" height="250" v-if="question.questionImage" :src="question.questionImage" />
     </div>
-    <div class="row text-center">
+    <div class="row text-center my-3">
       <h5>{{ question.questionText }}</h5>
     </div>
-    <div class="row text-center">
-      <div v-for="answer in question.possibleAnswers" v-bind:key="answer.date">
-        <button type="button" class="btn btn-primary main-btn my-2" @click="$emit('answer-selected', answer.id)">{{ answer.text }}</button>
-      </div>
+    <div class="row justify-content-center" v-for="answer in question.possibleAnswers" v-bind:key="answer.date">
+        <button type="button" class="col-4 btn btn-primary main-btn my-2" @click="$emit('answer-selected', answer.id)">{{ answer.text }}</button>
     </div>
     <div class="row">
 
