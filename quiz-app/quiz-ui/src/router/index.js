45fdac6988695137589a51c5d53635e94668e088
Maryline Fernandes <maryline.fernandes@edu.esiee.fr>
2023-05-09 15:38:22 +0200

    NewQuizPage component debut

--- quiz-app/quiz-ui/src/router/index.js
+++ quiz-app/quiz-ui/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
+import NewQuizPage from '../views/NewQuizPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,18 +10,10 @@ const router = createRouter({
       name: 'HomePage',
       component: HomePage
     },
-    // {
-    //   path: '/start-new-quiz-page',
-    //   name: 'Start new quiz',
-    //   component: NewQuizPage
-    // },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/start-new-quiz-page',
+      name: 'Start new quiz',
+      component: NewQuizPage
     }
   ]
 })
