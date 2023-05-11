eaff42aab7d2607c10e3fa6e3b87a58e94f7aa7c
MichelFrancisco <michel.francisco94000@gmail.com>
2023-05-11 11:36:41 +0200

    Ajout des effets on click rouge / vert

--- quiz-app/quiz-ui/src/utils/mouse.js
+++ quiz-app/quiz-ui/src/utils/mouse.js
@@ -5,5 +5,15 @@ document.addEventListener("click", function() {
     var audio = new Audio(audioFile);
     audio.volume = 0.15
     audio.play();
-
 });
+
+document.addEventListener('mousedown', function(e) {
+    var cursor = document.createElement('div');
+    cursor.className = 'click-cursor';
+    cursor.style.left = (e.pageX - 30) + 'px';
+    cursor.style.top = (e.pageY - 30) + 'px';
+    document.body.appendChild(cursor);
+    setTimeout(function() {
+      cursor.parentNode.removeChild(cursor);
+    }, 700);
+  });
