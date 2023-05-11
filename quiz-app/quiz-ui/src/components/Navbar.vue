d67a20a4a1e0070b19a3d00a69f5888803bd5997
Maryline Fernandes <maryline.fernandes@edu.esiee.fr>
2023-05-11 08:55:30 +0200

    navbar ok

---
+++ quiz-app/quiz-ui/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav main-navbar">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/"><i class="bi bi-house"></i></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login"><i class="bi bi-person"></i></router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    
+};
+</script>
