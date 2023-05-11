9cfc868bfe7cfdba152ccba255d0b1ef4129778b
Maryline Fernandes <maryline.fernandes@edu.esiee.fr>
2023-05-11 10:55:04 +0200

    dark mode / ligth ok

--- quiz-app/quiz-ui/src/components/Navbar.vue
+++ quiz-app/quiz-ui/src/components/Navbar.vue
@@ -7,6 +7,9 @@
             <router-link class="nav-link" to="/"><i class="bi bi-house"></i></router-link>
           </li>
           <li class="nav-item">
+            <button class="btn" @click="toggleTheme"><i class="bi bi-moon-stars"></i></button>
+          </li>
+          <li class="nav-item">
             <router-link class="nav-link" to="/login"><i class="bi bi-person"></i></router-link>
           </li>
         </ul>
@@ -18,6 +21,26 @@
 <script>
 export default {
     name: "Navbar",
+    methods:{
+      toggleTheme(){
+
+        if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+            document.documentElement.setAttribute('data-bs-theme','light');
+            window.localStorage.setItem('theme', 'light');
+        }
+        else {
+            document.documentElement.setAttribute('data-bs-theme','dark');
+            window.localStorage.setItem('theme', 'dark');
+        }
+      }
+    },
+    mounted(){
+      if( window.localStorage.getItem('theme') === undefined){
+          window.localStorage.setItem('theme', 'light');
+        }else{
+          document.documentElement.setAttribute('data-bs-theme', window.localStorage.getItem('theme'));
+        }
+    }
     
 };
 </script>
