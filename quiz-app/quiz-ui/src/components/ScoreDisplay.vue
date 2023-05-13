66a5a5557beb67fa9ec4999e172aa6487ecf5b07
Maryline Fernandes <maryline.fernandes@edu.esiee.fr>
2023-05-14 01:06:33 +0200

    badge player

--- quiz-app/quiz-ui/src/components/ScoreDisplay.vue
+++ quiz-app/quiz-ui/src/components/ScoreDisplay.vue
@@ -1,25 +1,35 @@
 <template>
-  <ul class="list-group" style="max-height: 230px;overflow: scroll;">
+  <ul class="list-group" style="max-height: 230px; overflow: scroll">
     <li class="list-group-item" v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
       {{ scoreEntry.playerName }} - {{ scoreEntry.score }} points
+      <span
+        v-if="scoreEntry.playerName == playerName && scoreEntry.score == score"
+        class="badge text-bg-success"
+        >TOI</span
+      >
     </li>
   </ul>
 </template>
 
 <script>
-import quizApiService from "@/services/QuizApiService";
+import quizApiService from '@/services/QuizApiService'
+import participationsStorageService from '../services/ParticipationStorageService'
 
 export default {
-name: "ScoreDisplay",
-data() {
+  name: 'ScoreDisplay',
+  data() {
     return {
-      registeredScores: []
-    };
+      registeredScores: [],
+      playerName: participationsStorageService.getPlayerName(),
+      score: participationsStorageService.getParticipationScore()
+    }
   },
   async created() {
-    let quizInfo = await quizApiService.getQuizInfo();
+    let quizInfo = await quizApiService.getQuizInfo()
 
-    this.registeredScores = quizInfo.data.scores;
+    this.registeredScores = quizInfo.data.scores
+    console.log(this.playerName)
+    console.log(this.score)
   }
-};
+}
 </script>
