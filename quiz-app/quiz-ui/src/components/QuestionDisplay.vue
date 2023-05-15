75028a4005347b023322e74486d8dee8b35ef09e
Maryline Fernandes <maryline.fernandes@edu.esiee.fr>
2023-05-15 14:46:26 +0200

    modifs front q°

--- quiz-app/quiz-ui/src/components/QuestionDisplay.vue
+++ quiz-app/quiz-ui/src/components/QuestionDisplay.vue
@@ -1,31 +1,43 @@
 <template>
-    <div class="row text-center">
-      <h3 class="main-title">{{ question.questionTitle}}</h3>
-    </div>
-    <div class="row text-center my-3 question-image" :style="{ 'background-image': 'url(' + question.questionImage + ')'}"></div>
-    <div class="row text-center my-4">
-      <h5>{{ question.questionText }}</h5>
-    </div>
-    <div class="row justify-content-center">
-      <button type="button" class="col-5 mx-2 btn btn-primary main-btn my-2" @click="$emit('answer-selected', i+1)" v-for="(answer, i) in question.possibleAnswers" v-bind:key="answer.date">
-          {{ answer.text }}
-      </button>
-    </div>
-    <div class="row">
-
-    </div>
-    
+  <div class="row text-center my-2">
+    <h3 class="main-title">{{ question.questionTitle }}</h3>
+  </div>
+  <div class="row text-center mt-3 mb-4 question-image">
+    <div
+      class="div"
+      :style="{
+        'background-position': 'center',
+        'background-size': 'contain',
+        'background-repeat': 'no-repeat',
+        'background-image': 'url(' + question.questionImage + ')'
+      }"
+    ></div>
+  </div>
+  <div class="row text-center mt-4 mb-5">
+    <h5>{{ question.questionText }}</h5>
+  </div>
+  <div class="row justify-content-center">
+    <button
+      type="button"
+      class="col-5 mx-2 btn btn-primary main-btn my-2"
+      @click="$emit('answer-selected', i + 1)"
+      v-for="(answer, i) in question.possibleAnswers"
+      v-bind:key="answer.date"
+    >
+      {{ answer.text }}
+    </button>
+  </div>
+  <div class="row"></div>
 </template>
 
 <script>
-
 export default {
-  name: "QuestionDisplay",
+  name: 'QuestionDisplay',
   props: {
-  question: {
-    type: Object
-  },
-  emits: ["answer-selected"]
+    question: {
+      type: Object
+    },
+    emits: ['answer-selected']
+  }
 }
-};
 </script>
--- quiz-app/quiz-ui/src/views/QuestionManager.vue
+++ quiz-app/quiz-ui/src/views/QuestionManager.vue
@@ -1,39 +1,52 @@
 <template>
   <div class="container">
-    <div class="row main-container">
-      <div class="col-12" style="height: 100%;padding-top: 2rem;">
+    <div class="row main-container justify-content-center">
+      <div class="col-8">
         <div class="row text-center">
           <div class="col-6">
-            <h4 class="main-title">Q° {{ currentQuestionPosition }} / {{ totalNumberOfQuestion }}</h4>
+            <span class="badge primary-badge main-title question-title-badge">
+              Q° {{ currentQuestionPosition }} / {{ totalNumberOfQuestion }}
+            </span>
           </div>
           <div class="col-6">
-            <h4 class="main-title">{{ username }}</h4>
+            <span class="badge primary-badge main-title question-title-badge">
+              {{ username }}
+            </span>
           </div>
         </div>
-        <div class="row text-center">
-          <QuestionDisplay :question="currentQuestion" @answer-selected="answerClickedHandler" />
+        <div class="card shadow border-0">
+          <div class="card-body">
+            <div class="offset-md-2 col-md-8 col-12" style="height: 100%; padding-top: 2rem">
+              <div class="row text-center">
+                <QuestionDisplay
+                  :question="currentQuestion"
+                  @answer-selected="answerClickedHandler"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>  
+  </div>
 </template>
 
 <script>
-import participationStorageService from "../services/ParticipationStorageService";
-import quizApiService from "../services/QuizApiService";
-import QuestionDisplay from "../components/QuestionDisplay.vue";
+import participationStorageService from '../services/ParticipationStorageService'
+import quizApiService from '../services/QuizApiService'
+import QuestionDisplay from '../components/QuestionDisplay.vue'
 
 export default {
-  name: "QuestionManager",
-  data(){
-    return{
-      currentQuestion:{
+  name: 'QuestionManager',
+  data() {
+    return {
+      currentQuestion: {
         questionTitle: '',
         questionText: '',
         questionImage: '',
         possibleAnswers: []
       },
-      currentQuestionPosition : 1,
+      currentQuestionPosition: 1,
       selectedAnswers: [],
       username: '',
       totalNumberOfQuestion: 0
@@ -43,36 +56,36 @@ export default {
     QuestionDisplay
   },
   async created() {
-    const quizInfo = await quizApiService.getQuizInfo();
-    this.username = participationStorageService.getPlayerName();
-    this.totalNumberOfQuestion = quizInfo.data.size;
-    this.loadQuestionByPosition();
+    const quizInfo = await quizApiService.getQuizInfo()
+    this.username = participationStorageService.getPlayerName()
+    this.totalNumberOfQuestion = quizInfo.data.size
+    this.loadQuestionByPosition()
   },
-  methods:{
-    async loadQuestionByPosition(){
-      let data = await quizApiService.getQuestionByPosition(this.currentQuestionPosition);
-      const question = data.data;
+  methods: {
+    async loadQuestionByPosition() {
+      let data = await quizApiService.getQuestionByPosition(this.currentQuestionPosition)
+      const question = data.data
 
-      this.currentQuestion.questionTitle = question.title;
-      this.currentQuestion.questionText = question.text; 
-      this.currentQuestion.questionImage = question.image;
-      this.currentQuestion.possibleAnswers = question.possibleAnswers;
+      this.currentQuestion.questionTitle = question.title
+      this.currentQuestion.questionText = question.text
+      this.currentQuestion.questionImage = question.image
+      this.currentQuestion.possibleAnswers = question.possibleAnswers
     },
-    async answerClickedHandler(answerId){
-      this.selectedAnswers.push(answerId);
-      if(this.currentQuestionPosition === this.totalNumberOfQuestion) {
-        this.endQuiz();
-      }else{
-        this.currentQuestionPosition += 1;
-        await this.loadQuestionByPosition();
+    async answerClickedHandler(answerId) {
+      this.selectedAnswers.push(answerId)
+      if (this.currentQuestionPosition === this.totalNumberOfQuestion) {
+        this.endQuiz()
+      } else {
+        this.currentQuestionPosition += 1
+        await this.loadQuestionByPosition()
       }
     },
-    async endQuiz(){
-      this.username = participationStorageService.getPlayerName();
+    async endQuiz() {
+      this.username = participationStorageService.getPlayerName()
       let data = await quizApiService.postAnswers(this.username, this.selectedAnswers)
-      participationStorageService.saveParticipationScore(data.data.score);
-      this.$router.push('/end-quiz');
+      participationStorageService.saveParticipationScore(data.data.score)
+      this.$router.push('/end-quiz')
     }
   }
-};
+}
 </script>
